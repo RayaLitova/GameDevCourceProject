f271a388e884bdc5be213b143fc91b86eadf743e
Raya Litova <raya.litova23@gmail.com>
2025-09-08 16:40:29 +0300

    different colors for element spells now work

--- Assets/Scripts/Spells/SpellBullet.cs
+++ Assets/Scripts/Spells/SpellBullet.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class SpellBullet : MonoBehaviour
@@ -5,6 +6,13 @@ public class SpellBullet : MonoBehaviour
     public ActiveSpell spell = null;
 	public int damage = 0;
 	public string hit_tag = "Enemy";
+
+	static Tuple<Color, Color>[] color = new Tuple<Color, Color>[] {
+		new Tuple<Color, Color>(Color.yellow, Color.red),
+		new Tuple<Color, Color>(Color.blue, Color.cyan),
+		new Tuple<Color, Color>(Color.green, Color.brown),
+		new Tuple<Color, Color>(Color.white, Color.gray),
+	};
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
@@ -27,8 +35,14 @@ public class SpellBullet : MonoBehaviour
 				GameObject effect = Instantiate(spell.effectPrefab, position, Quaternion.identity);
 				UnityEngine.VFX.VisualEffect vfx = effect.GetComponent<UnityEngine.VFX.VisualEffect>();
 				vfx.SetFloat("height", height);
+
 				var gradient = vfx.GetGradient("ColorGradient");
-				Debug.Log(gradient.colorKeys);
+				Debug.Log(spell.spellType);
+				var keys = gradient.colorKeys;
+				keys[0].color = color[(int)spell.spellType % color.Length].Item1;
+				keys[1].color = color[(int)spell.spellType % color.Length].Item2;
+				gradient.colorKeys = keys;
+				vfx.SetGradient("ColorGradient", gradient);
 
 				Destroy(effect, 2f);
 			} else {
